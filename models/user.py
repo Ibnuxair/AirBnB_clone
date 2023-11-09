c1f83a16901f92c842e8555be5c452e90f718299
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-11-09 04:51:20 -0800

    AirBnB_clone

--- models/user.py
+++ models/user.py
@@ -18,3 +18,21 @@ class User(BaseModel):
         self.password = ""
         self.first_name = ""
         self.last_name = ""
+
+    def __str__(self):
+        """ Returns the string representation of the object."""
+
+        attributes = {
+            'id': self.id,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'email': self.email,
+            'password': self.password,
+            'first_name': self.first_name,
+            'last_name': self.last_name
+        }
+
+        non_empty_attributes = {k: v for k, v in attributes.items() if v}
+
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, non_empty_attributes)
